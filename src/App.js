e79735f53734c9a70e7bd9b76152708035399308
ayoub <ayoub-ok147@hotmail.fr>
2023-08-03 11:47:49 +0100

    center

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 // import logo from './logo.svg';
 import "./App.css";
 import Navbar from "./NavBar";
-import ResponsiveAppBar from "./NavBar";
+// import ResponsiveAppBar from "./NavBar";
 import SurveyForm from "./SurveyForm";
 import Container from "@mui/material/Container";
 
 function App() {
   return (
     <div className="App">
-      {/* <ResponsiveAppBar/> */}
       <Navbar />
       
-      <Container maxWidth="md">
+      <Container maxWidth="md" style={{height:"85vh",display:"flex",justifyContent:"center",alignItems:"center"}}>
         <SurveyForm  />
       </Container>
     </div>
