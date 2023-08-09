25535e29196f95d6ab5108114943558eb6be4453
ayoub <ayoub-ok147@hotmail.fr>
2023-08-09 17:01:23 +0100

    sitCod

--- src/steps/Step2.jsx
+++ src/steps/Step2.jsx
@@ -58,7 +58,7 @@ function Step2({ data, myError, handleChange,setRecaptchaValue,handleRecaptchaCh
             defaultValue={data.phone}
             onChange={handleChange}
           />
-      <ReCAPTCHA name="captcha" sitekey="6LdNvJInAAAAAKv347uQ2qZ386qNjFaUB7h6TtE0"  onChange={handleChange} />
+      <ReCAPTCHA name="captcha" sitekey="6LeA3JInAAAAAISXYr1KCqJVSm93ICjhL05l2Zwt"  onChange={handleChange} />
         </form>
       </Box>
 
