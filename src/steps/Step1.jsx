eda30e0ad2bb2c69ef893f44aadff03dbb52309c
ayoub <ayoub-ok147@hotmail.fr>
2023-08-10 11:50:58 +0100

    gg

--- src/steps/Step1.jsx
+++ src/steps/Step1.jsx
@@ -4,7 +4,13 @@ import { TextField } from "@mui/material";
 // import TextField from './step/@mui/material/TextField';
 import ReCAPTCHA from "react-google-recaptcha";
 
-function Step1({ data,myError, handleChange ,setRecaptchaValue,handleRecaptchaChange}) {
+function Step1({
+  data,
+  myError,
+  handleChange,
+  setRecaptchaValue,
+  handleRecaptchaChange,
+}) {
   console.log(myError);
   return (
     <div
@@ -13,7 +19,7 @@ function Step1({ data,myError, handleChange ,setRecaptchaValue,handleRecaptchaCh
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
-        width:"100%"
+        width: "100%",
       }}
     >
       <h2>Enter User Details</h2>
@@ -24,14 +30,45 @@ function Step1({ data,myError, handleChange ,setRecaptchaValue,handleRecaptchaCh
         // }}
         noValidate
         autoComplete="off"
-        style={{width:"100%"}}
+        style={{ width: "100%" }}
       >
-        <form  style={{ display: "flex", flexDirection: "column" }}>
-          <TextField error={myError.firstName} style={{width:"100%"}} id="firstName" label="First Name" variant="standard" name="firstName" defaultValue={data.firstName} onChange={handleChange} />
-          <TextField error={myError.lastName} style={{width:"100%"}} id="lastName" label="Last Name" variant="standard" name="lastName"  defaultValue={data.lastName} onChange={handleChange}/>
-          <TextField error={myError.email} style={{width:"100%"}} id="email" label="Email" type="email" variant="standard" name="email"  defaultValue={data.email} onChange={handleChange}/>
-      <ReCAPTCHA name="captcha" sitekey="6Le9_pUnAAAAAPz57O4Vpgy4g3UjG6JICw9MCcLs"  onChange={handleChange} />
-
+        <form style={{ display: "flex", flexDirection: "column" }}>
+          <TextField
+            error={myError.firstName}
+            style={{ width: "100%" }}
+            id="firstName"
+            label="First Name"
+            variant="standard"
+            name="firstName"
+            defaultValue={data.firstName}
+            onChange={handleChange}
+          />
+          <TextField
+            error={myError.lastName}
+            style={{ width: "100%" }}
+            id="lastName"
+            label="Last Name"
+            variant="standard"
+            name="lastName"
+            defaultValue={data.lastName}
+            onChange={handleChange}
+          />
+          <TextField
+            error={myError.email}
+            style={{ width: "100%" }}
+            id="email"
+            label="Email"
+            type="email"
+            variant="standard"
+            name="email"
+            defaultValue={data.email}
+            onChange={handleChange}
+          />
+          <ReCAPTCHA
+            name="captcha"
+            sitekey="6Le9_pUnAAAAAPz57O4Vpgy4g3UjG6JICw9MCcLs"
+            onChange={handleChange}
+          />
         </form>
       </Box>
 
